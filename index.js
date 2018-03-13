574f9ae78212715596193b1f4c87fdff02dd3b88
Эрик <ernest.shelyag@gmail.com>
2018-03-13 12:50:59 +0300

    done

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
 const http = require('http');
+const stopTimer = 5000;
 
 const server = http.createServer(function (req, res) {
-  if (req.method !== 'GET') return res.end('It is not the correct method.\n');
-  const interval = +process.env.INT || 5000;
-  const exitPeriod = +process.env.EXT || 30000;
-  const exitTime = Date.now() + exitPeriod;
-  const printDate = () => {
-    const date = new Date().toUTCString();
-    console.log(date);
-    return date;
-  };
-  const checkTimeExit = () => {
-    const nowDate = Date.now();
-    const nextPrintTime = nowDate + interval;
-    if (nextPrintTime > exitTime) {
-      const leftTime = exitTime - nowDate;
-      clearInterval(timer);
-      setTimeout(() => {
-        const date = printDate();
-        res.end(date);
-      }, leftTime);
-    }
-  };
-  printDate();
-  const timer = setInterval(() => {
-    printDate();
-    checkTimeExit();
-  }, interval);
+  if (req.method === 'GET') {
+    const start = new Date();
+    let timer = setInterval(function () {
+      const date = new Date();
+      console.log(date);
+      if (date - start >= stopTimer) {
+        clearInterval(timer);
+        res.end('Start: ' + start.toUTCString() + '\nFinish: ' + date.toUTCString(), 'utf8');
+      }
+    }, 500);
+  }
 });
-server.listen(3000);
 
-server.on('error', (err) => {
-  console.error(err);
-  process.exit(1);
-});
+server.listen(3000);
